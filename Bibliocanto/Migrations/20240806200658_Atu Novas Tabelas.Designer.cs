db1faccf174a2d527006ae7e5ee413888d0110f8
Marcos Vinícius <marcosviniciusmvmv7@gmail.com>
2024-08-06 22:32:18 -0300

    0.0.3
    Criação do projeto react para o front end da aplicação web

--- Bibliocanto/Migrations/20240806200658_Atu Novas Tabelas.Designer.cs
+++ Bibliocanto/Migrations/20240806200658_Atu Novas Tabelas.Designer.cs
@@ -4,6 +4,7 @@ using Bibliocanto.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Bibliocanto.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240806200658_Atu Novas Tabelas")]
+    partial class AtuNovasTabelas
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Bibliocanto/obj/Debug/net8.0/Bibliocanto.AssemblyInfo.cs
+++ Bibliocanto/obj/Debug/net8.0/Bibliocanto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bibliocanto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec6bd65d1bd053a324ba566f8895b9aad566e09d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94342df10e0e91c26bbd94e37b8643d55d82f6e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bibliocanto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bibliocanto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
