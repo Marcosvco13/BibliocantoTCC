adf9776e41cba9aebd75cfe24a746d1f8e685f1f
Marcosvco13 <marcosviniciusmvmv7@gmail.com>
2024-08-14 16:04:18 -0300

    0.0.7
    melhora na model de livros, crianção da model genero

--- Bibliocanto/Models/Livros.cs
+++ Bibliocanto/Models/Livros.cs
@@ -5,14 +5,16 @@ namespace Bibliocanto.Models
     public class Livros
     {
         public int Id { get; set; }
-        public string Titulo { get; set; }
-        public string Descricao { get; set; }
-        //public string CaminhoImagem { get; set; }
-
+        public string? Titulo { get; set; }
+        public string? Descricao { get; set; }
+        public string? CaminhoImagem { get; set; }
+        public string? ISBN { get; set; }
 
 
         public int AutorId { get; set; }
-        public Autores Autores { get; set; }
+        public Autores? Autores { get; set; }
+        public int generoid { get; set; }
+        public Generos? Generos { get; set; }
 
     }
 }
--- Bibliocanto/obj/Debug/net8.0/Bibliocanto.AssemblyInfo.cs
+++ Bibliocanto/obj/Debug/net8.0/Bibliocanto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bibliocanto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03fdc57dfeed17d29593af810099d3d1bbcf8636")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0c535900cff8ae938b55a878cf809563a511dd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bibliocanto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bibliocanto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
