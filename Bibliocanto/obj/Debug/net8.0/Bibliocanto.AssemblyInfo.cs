9d05f08aef5b4390f483b2cd842e9dcd6528de4f
Marcos Vinícius <marcosviniciusmvmv7@gmail.com>
2024-08-15 22:28:26 -0300

    0.1.0

--- Bibliocanto/obj/Debug/net8.0/Bibliocanto.AssemblyInfo.cs
+++ Bibliocanto/obj/Debug/net8.0/Bibliocanto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bibliocanto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3969c1b68805f03033737f4154b3130e8ad609c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d66291ca01bad2a8c2f1870b2f485482e8e3bc8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bibliocanto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bibliocanto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
