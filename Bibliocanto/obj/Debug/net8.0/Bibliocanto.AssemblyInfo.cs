23d2dc19be60afd8a7a85071a5e6db13aadcdfaf
Marcos Vinícius <marcosviniciusmvmv7@gmail.com>
2024-10-11 20:07:12 -0300

    3.0.4
    Finalizando com as controllers do autoreslivro e generoslivro

--- Bibliocanto/obj/Debug/net8.0/Bibliocanto.AssemblyInfo.cs
+++ Bibliocanto/obj/Debug/net8.0/Bibliocanto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bibliocanto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+122372d02a2474781c428d5276c40237274c7b98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39b536d6ca9258172e3c8d36f7f92c1c5a44afd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bibliocanto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bibliocanto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
