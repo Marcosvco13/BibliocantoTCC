757eddd93e8a9c262009a704ffeccd647abf5eef
Marcos Vinícius <marcosviniciusmvmv7@gmail.com>
2025-02-26 19:45:39 -0300

    Ajuste

--- Bibliocanto/obj/Debug/net8.0/Bibliocanto.AssemblyInfo.cs
+++ Bibliocanto/obj/Debug/net8.0/Bibliocanto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bibliocanto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faef6f40051a3eaa5e2b609598a1a8255447ff73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dffe5571be7c161cac003ef263a29c3d0a243216")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bibliocanto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bibliocanto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BibliocantoFront/BibliocantoTCC-MainFront/src/services/api.js
+++ BibliocantoFront/BibliocantoTCC-MainFront/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5162'
+    baseURL: 'https://localhost:44331'
 });
 
 api.validarToken = async () => {
