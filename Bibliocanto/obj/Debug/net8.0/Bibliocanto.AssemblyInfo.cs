bdbd59a2388a050463057eb380365c6a7003686f
Marcos Vinícius <marcosviniciusmvmv7@gmail.com>
2025-04-21 20:19:09 -0300

    Ajuste

--- Bibliocanto/obj/Debug/net8.0/Bibliocanto.AssemblyInfo.cs
+++ Bibliocanto/obj/Debug/net8.0/Bibliocanto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bibliocanto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bd34efc535c67d6e6e3bbd310dcfeb0b2d9e3d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f9ea9c9d450029dd1e67f988159652f036c45f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bibliocanto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bibliocanto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BibliocantoApp/services/api.ts
+++ BibliocantoApp/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://e3ee-2804-56c-d714-3b00-489a-32fe-2db5-f6df.ngrok-free.app/",
+    baseURL: "https://2daa-132-255-111-253.ngrok-free.app/",
 });
 
 export default api;
