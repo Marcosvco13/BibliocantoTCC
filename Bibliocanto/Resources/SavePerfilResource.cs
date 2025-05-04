4e168e329a1561abfadcd46ccf4b64be0501fbde
Marcos Vinícius <marcosviniciusmvmv7@gmail.com>
2025-05-03 22:15:54 -0300

    1.0.0

--- Bibliocanto/Resources/SavePerfilResource.cs
+++ Bibliocanto/Resources/SavePerfilResource.cs
@@ -5,11 +5,11 @@ namespace Bibliocanto.Resources
     public class SavePerfilResource
     {
         [StringLength(450)]
-        public string IdUser { get; set; }
-        public string Nome { get; set; }
-        public string Apelido { get; set; }
-        public string Descricao { get; set; }
+        public string? IdUser { get; set; }
+        public string? Nome { get; set; }
+        public string? Apelido { get; set; }
+        public string? Descricao { get; set; }
         public DateTime? DataNasc { get; set; }
-        public string FotoPerfil { get; set; }
+        public string? FotoPerfil { get; set; }
     }
 }
--- Bibliocanto/obj/Debug/net8.0/Bibliocanto.AssemblyInfo.cs
+++ Bibliocanto/obj/Debug/net8.0/Bibliocanto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bibliocanto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+733ab46880282ee2f8d28610b62a2ccde437406f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83d93be035df4df3c7c81ce8c0b975791ed08860")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bibliocanto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bibliocanto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
